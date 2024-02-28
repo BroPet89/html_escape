f01a7579048756271436a510c85317cfa8298ea0
Sean Thomas <sean.thomas@wealthify.com>
2024-02-28 15:43:16 +0000

    modify

--- parser/main.py
+++ parser/main.py
@@ -1,27 +1,44 @@
 from bs4 import BeautifulSoup
+import bs4
 import re
 
-# Open the HTML file in read mode
+
 with open(r'C:\dev\WealthifyMainV2\src\services\CashSavings\templates\statements\template.html', 'r', encoding='utf-8') as file:
-    # Read the contents of the file
     html_content = file.read()
 
-# Define a function to escape special characters
-def escape_special_characters(text):
-    # Escape backslashes and double quotes for JavaScript strings
-    escaped_text = text.replace('\\', '\\\\').replace('"', '\\"')
-    return escaped_text
-
-# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Find all elements with text content and escape special characters
-for element in soup.find_all(text=True):
-    element.replace_with(escape_special_characters(element))
+doctype_tag = soup.find(text=lambda text: isinstance(text, bs4.Doctype))
+if doctype_tag:
+    doctype_string = str(doctype_tag)
+    doctype_tag.extract()
+    soup.insert(0, doctype_string)
 
-# Write the modified HTML content to a new file
+
+def escape_special_characters_for_json(text):
+
+    text = text.replace('"', '\\"')
+
+    # Escape forward slash in HTML end tags
+    text = text.replace('</', '<\\/')
+
+    # Add a space between the tag name and the slash on self-closing tags
+    text = re.sub(r'(<\w+)\s*\/>', r'\1 />', text)
+
+    # Encode quotation marks within HTML content
+    text = text.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+
+    return text
+
+
+# Find all elements with text content and escape special characters for JSON
+for element in soup.find_all(text=True):
+    element.replace_with(escape_special_characters_for_json(element))
+
+html_content_single_line = soup.prettify().replace('\n', '').replace('  ', '')
+
 output_file_path = r'C:\dev\WealthifyMainV2\src\services\CashSavings\templates\statements\modified_template.html'
 with open(output_file_path, 'w', encoding='utf-8') as output_file:
-    output_file.write(soup.prettify())
+    output_file.write(html_content_single_line)
 
 print(f"Modified HTML content has been written to {output_file_path}")
